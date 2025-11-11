41cbea699dea9141f68c60eaab33d70738a9d91a
Paul Dale <ldpercy@gmail.com>
2025-11-11 18:57:41 +1100

    sweeper script

---
+++ wiki/linux/sweeper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# A bunch of places that get crufted up:
+
+
+rm ~/.cache/vlc/art/*
+
+rm ~/.cache/drkonqi/crashes/*
+rm ~/.cache/drkonqi/application-not-responding/*
+
+rm ~/.cache/thumbnails/*/*
+
+rm ~/.local/share/recently-used.xbel
+
+rm ~/.local/share/okular/docdata/*
+
+
