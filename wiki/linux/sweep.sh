584d566cafc46b024b89d8100b5fa701f452e613
Paul Dale <ldpercy@gmail.com>
2026-04-09 18:56:05 +1000

    Update sweep script

--- wiki/linux/sweep.sh
+++ wiki/linux/sweep.sh
@@ -11,9 +11,17 @@ rm ~/.cache/drkonqi/application-not-responding/*
 
 rm ~/.cache/thumbnails/*/*
 
+rm ~/.config/kfindrc
+## rm  ~/.config/QtProject.conf
+
+
+
 rm ~/.local/share/recently-used.xbel
 rm ~/.local/share/user-places.xbel
-
+rm ~/.local/share/RecentDocuments/*
+rm ~/.local/share/kwrite/anonymous.katesession
+rm ~/.local/state/kwritestaterc
+rm ~/.log/vlc.log
 
 ## rm ~/.local/share/okular/docdata/*
 
